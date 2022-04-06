0fa718ac1f36f2cd06dff4306c9f26f2854bc195
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-06 19:43:12 +0200

    ADD Input Component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import classes from "./Input.module.css"
 
 const Input = props => {
     return (
-        <div>Input</div>
+        <div className={classes.input}>
+            <label htmlFor={props.id}>{props.label}</label>
+            <input {...props.input} />
+        </div>
     )
 }
 

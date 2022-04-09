999b19b2e66eee8018590432771669dd0e576cc0
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-09 06:20:46 +0200

    Add bump animation when cart cout changes

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,18 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../../Cart/CartIcon'
 import classes from "./HeaderCartButton.module.css"
 import CartContext from '../../../store/cart-context';
 
 
 const HeaderCartButton = props => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
     const cartCtx = useContext(CartContext);
-    const numberOfCartItems = cartCtx.items.reduce((accum, item) => {
+    
+    //extract cart items
+    const { items: cartItems } = cartCtx;
+
+    const numberOfCartItems = cartItems.reduce((accum, item) => {
         return Math.abs(accum + item.amount)
     }, 0)
 
+    const btnClass = `${classes.button} ${btnIsHighlighted ? classes.bump : ""} `
+
+    useEffect(() => {
+        if (cartItems.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        //remove animation class because the component will be exute again whrn th state changes
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false)
+        }, 300);
+
+        //clear timer on destroy
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [cartItems])
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClass} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>

2d362ab431975e62b3d60387bffa45c167beb8af
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-12 23:34:51 +0200

    Add handle Error state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,11 +7,17 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
 
     useEffect(() => {
         const fetchMeals = async () => {
             setIsLoading(true)
             const response = await fetch("https://foodorder-2efd7-default-rtdb.firebaseio.com/meals.json")
+
+            if (!response.ok) {
+                throw new Error("somthing went wrong")
+            }
             const responseData = await response.json();
 
             const loadedMeals = [];
@@ -21,13 +27,26 @@ const AvailableMeals = () => {
             setMeals(loadedMeals)
             setIsLoading(false)
         }
-        fetchMeals()
+
+        fetchMeals().catch(error => {
+            setIsLoading(false)
+            setHttpError(error.message)
+        })
+
     }, [])
 
     if (isLoading) {
         return (
             <section className={classes.MealsLoading}>
                 <p>loading...</p>
+            </section>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
             </section>
         )
     }
